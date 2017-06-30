9cda11d831d852995159e5d0c94f77af242014ab
Daemon <Daemon@10.211.55.5>
2017-06-30 12:37:20 +0800

    plz let me commit

--- src/com/article/controller/ArticleServlet.java
+++ src/com/article/controller/ArticleServlet.java
@@ -15,7 +15,7 @@ public class ArticleServlet extends HttpServlet {
 			throws ServletException, IOException {
 		doPost(req, res);
 	}
-
+//e04
 	public void doPost(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
 
